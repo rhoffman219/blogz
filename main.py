12e876191c6822d4955322eab76732bf7b47a413
Richard Hoffman <rhoffman219@gmail.com>
2017-10-12 12:22:44 -0500

    made adjustments to def index() and now currently renders blog.html. newpost.html minor adjustments made, form now not rendering and throwing an error.

--- main.py
+++ main.py
@@ -1,103 +1,90 @@
 from flask import Flask, request, redirect, render_template
 
-from flask_sqlalchemy import SQLAlchemy
 
 
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
-
 app.config['SQLALCHEMY_ECHO'] = True
-
 db = SQLAlchemy(app)
-
-
-
-
 
 class Blog(db.Model):
 
-
-
     id = db.Column(db.Integer, primary_key=True)
-
     title = db.Column(db.String(120))
-
-    body = db.Column(db.String(120))
-
-
+    body = db.Column(db.String(20000))
 
     def __init__(self, title, body):
 
         self.title = title
+
         self.body = body
 
-    def __repr__(self):
-        return '<title %r>' % self.title
-
-    def __repr__(self):
-        return '<body %r>' % self.body
-
-
-def get_current_blogs():
-    return Blog.query.filter_by(title=True).all()
-
-
-@app.route('/blog', methods=['GET', 'POST'])
-def blog():
-    #use a 'GET' to go to the database to retreive the current submitted Blogs so they will be displayed on the main blog page. this is just a list of the blogs.
-    title_name = request.form['title']
-
-    body_content = request.form['body']
-
-    new_tile = Blog(title_name)
-
-    new_body = Blog(body_content)
-
-    db.session.add(new_title, body_content)
-
-    db.session.commit()
-
-    blogs = Task.query.filter_by(title=False).all()
-    completed_blogs = Task.query.filter_by(title=True).all()
-
-@app.route('/newpost', methods=['POST'])
-def new_post():
+@app.route('/', methods=['POST', 'GET'])
+def index():
     if request.method == 'POST':
+        title_name = request.form['title']
+        new_title = Blog(title_name)
+        db.session.add(new_title)
+        db.session.commit()
 
+    titles = Blog.query.all()
+    return render_template('blog.html',title="Blogs!", titles=titles)
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+    if request.method == "POST":
+        title_name = request.form['title']
+        body_content = request.form['body']
+        new_title = Blog(title_name)
+        new_body_content = Blog(body_content)
+        db.session.add(new_title, new_body_content)
+        db.session.commit()
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def new_post():
+
+    if request.method == 'POST': 
         title = request.form['title']
         body = request.form['body']
         title_err = ''
-        body_err = ''
+        body_err = ''   
 
-        if new_post_title == '':
+        if title == '':
+
             title_err = "Please enter a valid Title"
-    
-        if new_post_body == '':
+
+        elif body == '':
+
             body_err = "Please enter a valid blog post"
 
+        if not title_err or not body_err:
+            blog = Blog(title, body)
+
+            db.session.add(blog)
+
+            db.session.commit()
+
+            return render_template('blog.html', title=title, body=body)   
+        
     
+    
+        else:
 
-        return render_template('newpost.html')
+            return render_template('newpost.html', title_err=title_err, body_err=body_err, title=title, body=body)
 
 
-    else:
-        blog = Blog(title, body)
-        db.session.add(blog)
-        db.session.commit()
 
-        return redirect('/blog', title=title, body=body, body_err=body_err, title_err=title_err)
         
 
-    @app.route('/')
-    def index():
-        encoded_error = request.args.get("error")
-        return render_template('blog.html', encoded_error=error, blog_list=get_current_blogs())
+
+
 
 
 if __name__ == '__main__':
+
+
 
     app.run()
